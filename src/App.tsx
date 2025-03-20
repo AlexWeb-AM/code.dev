40e624cda5a35e5a5734d51f956cae314d3c69fd
Alex <balyanalex272@gmail.com>
2025-03-20 23:20:28 +0400

    .

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import { Register } from "./pages/auth/Register"
 import { ResetPassword } from "./pages/auth/ResetPassword"
 import { OtpCode } from "./pages/auth/OtpCode"
 import { ChangePassword } from "./pages/auth/ChangePassword"
+import { Toaster } from "react-hot-toast"
 
 export const App = () => {
   return (
     <Router>
-    <>
+      <Toaster position="top-center"
+  reverseOrder={false} />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
@@ -16,7 +18,6 @@ export const App = () => {
         <Route path="/otp-code-verify" element={<OtpCode />} />
         <Route path="/change-password" element={<ChangePassword />} />
       </Routes>
-    </>
     </Router>
   )
 }
--- src/components/auth/RegisterComponent.tsx
+++ src/components/auth/RegisterComponent.tsx
@@ -20,7 +20,7 @@ export const RegisterComponent = () => {
 
       try {
         const response =  await dispatch(registerUser({name,email,password})).unwrap()
-        toast.success('Succesfully Created Account')
+        toast.success('Succesfully Created Account',{style:{background:'#020202',color:'#fff'}})
         localStorage.setItem('name',name)
 
         navigate(`/user/${response.user.routeId}`)  
@@ -29,14 +29,14 @@ export const RegisterComponent = () => {
       } catch (err: unknown) {
         if (axios.isAxiosError(err) && err.response) {
             if (err.response.status === 400) {
-                toast.error("That e-mail is already registered");
+                toast.error("That e-mail is already registered",{style:{background:'#020202',color:'#fff'}});
             } else {
-                toast.error(err.response.data?.message || "Error");
+                toast.error(err.response.data?.message || "Error",{style:{background:'#020202',color:'#fff'}});
             }
         } else if (err instanceof Error) {
-            toast.error(err.message || "Error");
+            toast.error(err.message || "Error",{style:{background:'#020202',color:'#fff'}});
         } else {
-            toast.error("Unknown error occurred");
+            toast.error("Unknown error occurred",{style:{background:'#020202',color:'#fff'}});
         }
     }
 
@@ -46,8 +46,9 @@ export const RegisterComponent = () => {
     <div className="w-[390px] h-[480px] bg-none border-[1.5px] rounded-xl pl-6 border-neutral-800 ">
         <h3 className="text-white text-2xl font-semibold mt-7">Register</h3>
         <p className="mt-1.5 text-neutral-400 text-sm">Create an account to use the app</p>
+        <form onSubmit={handleSubmit}>
         <div className="flex flex-col gap-6 mt-7">
-          <form onSubmit={handleSubmit}>
+          
         <div className="grid gap-2">
             <label htmlFor="name" className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Full Name</label>
             <input required value={name} onChange={(e) => setName(e.target.value) }  type="text" placeholder="John Smith" id="name" className="border-[1px] border-neutral-500 w-[340px] px-2 py-2 rounded-md focus-visible:outline-1 focus-visible:border-neutral-100  bg-transparent placeholder:text-sm "  />
@@ -64,11 +65,13 @@ export const RegisterComponent = () => {
           <div className="">
             <button type="submit" disabled={isLoading} className="bg-neutral-50 text-sm cursor-pointer text-black w-[340px] h-10 rounded-md transition-opacity hover:opacity-80">Register</button>
           </div>
-          </form>
+          
           <div className="mt-[-5px] w-[340px] text-center div_signup_text text-sm">
           Already have an account?<Link to='/' className="underline underline-offset-2 ml-1">Login</Link>
           </div>
+          
         </div>
+        </form>
         
         
     </div>
