5ac7c35b1f14176ab1c480075af9d0b5396df20e
Alex <balyanalex272@gmail.com>
2025-03-21 21:53:19 +0400

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router,Routes,Route } from "react-router-dom"
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom"
 import { Login } from "./pages/auth/Login"
 import { Register } from "./pages/auth/Register"
 import { ResetPassword } from "./pages/auth/ResetPassword"
