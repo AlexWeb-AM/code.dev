7c75175a4bd8ce7a32d5879a7e5c047f6edf05de
Alex <balyanalex272@gmail.com>
2025-03-21 20:21:11 +0400

    .

--- src/slices/authSlice.ts
+++ src/slices/authSlice.ts
@@ -1,7 +1,7 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_URL_LOCAL = "http://localhost:5000/api/auth";
+const API_URL_LOCAL = "https://coder-dev-server.onrender.com";
 
 interface User {
   id: string;
