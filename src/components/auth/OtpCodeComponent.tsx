00174a92547913c3d85981f4c6756424891e0b3d
Alex <balyanalex272@gmail.com>
2025-03-21 22:20:33 +0400

    Send again functional

--- src/components/auth/OtpCodeComponent.tsx
+++ src/components/auth/OtpCodeComponent.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../store/store";
 import { useNavigate } from "react-router-dom";
-import { checkOtp } from "../../slices/authSlice";
+import { checkOtp, sendOtp } from "../../slices/authSlice";
 import toast from "react-hot-toast";
 
 export const OtpCodeComponent = () => {
@@ -33,7 +33,6 @@ export const OtpCodeComponent = () => {
             if (!otp[index] && index > 0) {
                 inputRefs.current[index - 1]?.focus();
             }
-
             const newOtp = [...otp];
             newOtp[index] = "";
             setOtp(newOtp);
@@ -46,16 +45,14 @@ export const OtpCodeComponent = () => {
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-
         try {
             const email = localStorage.getItem("email");
-
             if (!email) {
                 toast.error("Email not found. Please try again.");
                 return;
             }
 
-            const otpCode = otp.join(""); 
+            const otpCode = otp.join("");
             if (otpCode.length !== 6) {
                 toast.error("Please enter a valid 6-digit OTP.");
                 return;
@@ -71,13 +68,31 @@ export const OtpCodeComponent = () => {
         }
     };
 
+    const handleAgainSend = async () => {
+        try {
+            const email = localStorage.getItem("email");
+            if (!email) {
+                toast.error("Email not found. Please try again.");
+                return;
+            }
+
+            await dispatch(sendOtp({ email })).unwrap();
+            toast.success("OTP sent again.");
+        } catch (err) {
+            toast.error("Unexpected error occurred", {
+                style: { background: "#020202", color: "#fff" },
+            });
+            console.log(err);
+        }
+    };
+
     return (
-        <div className="w-[390px] h-[270px] border-[1.5px] rounded-xl pl-6 border-neutral-800 max-sm:scale-75">
+        <div className="w-[390px] h-[290px] border rounded-xl pl-6 border-neutral-800 max-sm:scale-75">
             <h3 className="text-white text-2xl font-semibold mt-7">Verify Email</h3>
             <p className="mt-1.5 text-neutral-400 text-sm">Enter the code that came to your email</p>
             <form onSubmit={handleSubmit}>
                 <div className="flex flex-col gap-6 mt-7">
-                    <div className="flex inputs_div">
+                    <div className="flex">
                         {otp.map((digit, index) => (
                             <input
                                 key={index}
@@ -90,7 +105,7 @@ export const OtpCodeComponent = () => {
                                 ref={(el) => {
                                     if (el) inputRefs.current[index] = el;
                                 }}
-                                className="first:rounded-tl-lg first:rounded-bl-lg last:rounded-tr-lg last:rounded-br-lg w-14 h-14 text-lg border-neutral-700 border-1 text-center focus-visible:outline-none"
+                                className="first:rounded-tl-lg first:rounded-bl-lg last:rounded-tr-lg last:rounded-br-lg w-14 h-14 text-lg border-neutral-700 border text-center focus-visible:outline-none"
                             />
                         ))}
                     </div>
@@ -101,6 +116,18 @@ export const OtpCodeComponent = () => {
                     >
                         Verify Email
                     </button>
+
+                    <div className="mt-[-5px] w-[340px] text-center text-sm">
+                        The letter didn't arrive?
+                        <button
+                            disabled={isLoading}
+                            type="button"
+                            onClick={handleAgainSend}
+                            className="underline underline-offset-2 ml-1 cursor-pointer disabled:text-neutral-900"
+                        >
+                            Send again
+                        </button>
+                    </div>
                 </div>
             </form>
         </div>
